dfbb75309621e3e374c0ee0ce27687ee1c351f09
Mufasa007 <594564264@qq.com>
2022-03-26 15:50:49 +0800

    [ADD]:新增结题Solution190

---
+++ src/leetcode/com/Solution190.java
@@ -0,0 +1,45 @@
+package src.leetcode.com;
+
+/**
+ * @Author 59456
+ * @Date 2022/3/26
+ * @Descrip
+ * @Version 1.0
+ */
+public class Solution190 {
+
+
+
+    public static void main(String[] args) {
+        int n= 964176192;
+        System.out.println(Integer.toBinaryString(n));
+        int out = reverseBits(n);
+        System.out.println(Integer.toBinaryString(out));
+    }
+
+    public static int reverseBits1(int n) {
+        int out = 0;
+        for (int i = 0; i < 32 && n!=0; i++) {
+            out |= (n&1)<<(31-i);
+            n>>>=1;
+        }
+        return out;
+    }
+
+    public static int reverseBits(int n) {
+        n = n >>> 1 & M1 | (n & M1) << 1;
+        n = n >>> 2 & M2 | (n & M2) << 2;
+        n = n >>> 4 & M4 | (n & M4) << 4;
+        n = n >>> 8 & M8 | (n & M8) << 8;
+        return n >>> 16 | n << 16;
+    }
+    private static final int M1 = 0x55555555; // 01010101010101010101010101010101
+    private static final int M2 = 0x33333333; // 00110011001100110011001100110011
+    private static final int M4 = 0x0f0f0f0f; // 00001111000011110000111100001111
+    private static final int M8 = 0x00ff00ff; // 00000000111111110000000011111111
+
+
+
+
+
+}
