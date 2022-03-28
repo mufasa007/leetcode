eb91b764afa65d369a0058c08efa78da05e42960
wanyu <594564264@qq.com>
2022-03-28 14:16:53 +0800

    [ADD]:146. LRU 缓存 直接使用LinkedHashMap进行实现

--- src/leetcode/com/Solution146.java
+++ src/leetcode/com/Solution146.java
@@ -26,6 +26,28 @@ public class Solution146 {
         }
     }
 
+    static class LRUCache1 extends LinkedHashMap<Integer,Integer>{
+        private int capacity;
+
+        public LRUCache1(int capacity) {
+            super(capacity,0.75F,true);
+            this.capacity = capacity;
+        }
+
+        public int get(int key) {
+            return super.getOrDefault(key,-1);
+        }
+
+        public void put(int key, int value) {
+            super.put(key,value);
+        }
+
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+            return size()>capacity;
+        }
+    }
+
     static class LRUCache {
 
         private int capacity;
