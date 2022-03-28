ce83a23d9f5fddda01018a3dac48d7d33085c031
wanyu <594564264@qq.com>
2022-03-28 12:26:19 +0800

    [ADD]:120. 三角形最小路径和

--- src/leetcode/com/Solution120.java
+++ src/leetcode/com/Solution120.java
@@ -12,8 +12,24 @@ public class Solution120 {
         triangle.add(Arrays.asList(3,4));
         triangle.add(Arrays.asList(6,5,7));
         triangle.add(Arrays.asList(4,1,8,3));
-        int i = solution.minimumTotal(triangle);
+        int i = solution.minimumTotal1(triangle);
         System.out.println();
+    }
+
+    // 优化一下
+    public int minimumTotal1(List<List<Integer>> triangle) {
+        int h = triangle.size();
+        int[][] dp = new int[h][h];
+        dp[0][0] = triangle.get(0).get(0);
+
+        for (int i = 1; i < h; i++) {
+            dp[i][0] = dp[i-1][0] + triangle.get(i).get(0);
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i - 1][j], dp[i-1][j - 1]);
+            }
+            dp[i][i] = dp[i-1][i - 1]+triangle.get(i).get(i);
+        }
+        return Arrays.stream(dp[h-1]).min().getAsInt();
     }
 
     public int minimumTotal(List<List<Integer>> triangle) {
