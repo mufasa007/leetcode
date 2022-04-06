7feeebc1277a5ea5606dc56cf78b42ebb96cbcec
wanyu <594564264@qq.com>
2022-04-06 12:22:08 +0800

    [ADD]:762. 二进制表示中质数个计算置位

---
+++ src/leetcode/com/Solution22.java
@@ -0,0 +1,75 @@
+package src.leetcode.com;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution22 {
+    public static void main(String[] args) {
+        Solution22 solution = new Solution22();
+
+        System.out.println();
+    }
+
+
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<String>();
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
+
+    public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
+        if (cur.length() == max * 2) {
+            ans.add(cur.toString());
+            return;
+        }
+        if (open < max) {
+            cur.append('(');
+            backtrack(ans, cur, open + 1, close, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+        if (close < open) {
+            cur.append(')');
+            backtrack(ans, cur, open, close + 1, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
+
+
+    // 暴力求解,太暴力了！穷举法
+    public List<String> generateParenthesis1(int n) {
+        List<String> res = new ArrayList<>();
+        generateAll(new char[2 * n], 0, res);
+        return res;
+    }
+
+    private void generateAll(char[] current, int pos, List<String> result) {
+        if (pos == current.length) {
+            if (valid(current)) {
+                result.add(new String(current));
+            } else {
+
+
+                current[pos] = '(';
+                generateAll(current, pos + 1, result);
+                current[pos] = ')';
+                generateAll(current, pos + 1, result);
+            }
+        }
+    }
+
+    private boolean valid(char[] current) {
+        int balance = 0;
+        for (char c : current) {
+            if (c == '(') {
+                balance++;
+            } else {
+                balance--;
+            }
+            if (balance < 0) {
+                return false;
+            }
+        }
+        return balance == 0;
+    }
+
+}
