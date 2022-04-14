e5e6f6922f817562d6786b6df5635663c947b2ce
Mufasa007 <594564264@qq.com>
2022-04-14 19:15:02 +0800

    [ADD]:744. 寻找比目标字母大的最小字母 三种解法

--- src/leetcode/com/Solution744.java
+++ src/leetcode/com/Solution744.java
@@ -4,11 +4,13 @@ public class Solution744 {
     public static void main(String[] args) {
         Solution744 solution = new Solution744();
         char[] letters = {'c', 'f', 'j'};
-        char target = 'a';
+        char target = 'j';
         char c = solution.nextGreatestLetter(letters, target);
+        char c1 = solution.nextGreatestLetter1(letters, target);
         System.out.println();
     }
 
+    // 暴力解法
     public char nextGreatestLetter(char[] letters, char target) {
         for (char letter : letters) {
             if (letter > target) {
@@ -16,5 +18,25 @@ public class Solution744 {
             }
         }
         return letters[0];
+    }
+
+    public char nextGreatestLetter1(char[] letters, char target) {
+        int len = letters.length;
+        if(letters[len-1]<=target){
+            return letters[0];
+        }
+
+        int l = 0 ,r = len-1;
+        int mide = 0;
+        while (l<=r){
+            mide = l +((r-l)>>1);
+            if(letters[mide]<=target){
+                l = mide+1;
+                mide = l;
+            }else {
+                r = mide-1;
+            }
+        }
+        return letters[mide];
     }
 }
