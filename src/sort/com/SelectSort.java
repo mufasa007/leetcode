3a7ce45305bbc0c29a79ef16c6899dd24a0735b9
Mufasa007 <594564264@qq.com>
2022-04-24 20:25:35 +0800

    [ADD]:选择排序优化-减少1倍的遍历次数

--- src/sort/com/SelectSort.java
+++ src/sort/com/SelectSort.java
@@ -31,4 +31,42 @@ public class SelectSort extends Sort{
         }
 
     }
+
+    /*
+     * 减少1倍的遍历次数
+     */
+    void sort1(int[] nums) {
+        if(nums == null){
+            return;
+        }
+        int len = nums.length;
+
+        int l=0,r=len-1;
+        while (l<=r){
+            int min=nums[l],max=nums[l],pre=nums[l] ;
+            int minIndex=l,maxIndex=l;
+            for (int i = l; i <= r ; i++) {
+                if(min>nums[i]){
+                    min=nums[l];
+                    minIndex=i;
+                }
+                if(max<nums[i]){
+                    max=nums[i];
+                    maxIndex=i;
+                }
+            }
+            swap(nums,l,minIndex);
+            swap(nums,r,maxIndex);
+            l++;
+            r--;
+        }
+    }
+
+    public static void main(String[] args) {
+        SelectSort selectSort = new SelectSort();
+        int[] ints = {5, 4, 3, 6, 8, 6, 9};
+        selectSort.sort1(ints);
+        System.out.println();
+    }
+
 }
